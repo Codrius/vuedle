0cb59fc6ecd4584ff530db5ec7b2e40ef3d8e7ae
Codrius <magefire342@gmail.com>
2023-08-09 14:28:39 -0400

    Initial Commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav>
+        <router-link to="HomePage"> </router-link>
+    </nav>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
