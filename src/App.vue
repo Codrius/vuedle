ce34d5ca5655a7799886244f83fbfc495360e861
Codrius <magefire342@gmail.com>
2023-08-09 15:07:48 -0400

    Still minor, still not working

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-    <Navbar/>
+    <NavBar/>
     <router-view/>
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
+import NavBar from './components/Navbar.vue'
+
+export default {
+  components: NavBar
+}
 </script>
 
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,9 @@
 </template>
 
 <script>
-
+export default {
+    name: "NavBar"
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
