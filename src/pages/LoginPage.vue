dc3985066a441706346d3afdf289bfce3636a913
Codrius <magefire342@gmail.com>
2023-08-09 16:42:00 -0400

    Implemented Login Page

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    <h1>This is my Login page</h1>
+  <div class="Login">
+    <h1>Welcome to Vuedle!  Please log in:</h1>
+    <label for="Username">Username: </label>
+    <input class="Username" v-model="username"/>
+    <br>
+    <label for="Password">Password: </label>
+    <input class="Password" v-model="password"/>
+    <br>
+    <button v-on:click="attemptLogin">Login</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() { return {
+    username: "",
+    password: ""
+  }},
+  methods: {
+    attemptLogin: function() {
+      console.log(this.username);
+      console.log(this.password);
+    }
+  }
+}
+</script>
