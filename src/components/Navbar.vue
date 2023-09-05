d5990e1125823c91000c635037b1654085255d98
Codrius <magefire342@gmail.com>
2023-09-05 19:37:37 -0400

    Adjusted navbar and clickable icon Navbar brand has an image now and has a right align Game clickable image changes cursor and is not draggable or selectable

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,27 @@
 <template>
   <b-navbar class="px-3" toggleable="sm" type="dark" variant="dark">
-    <b-navbar-brand active-class="active" to="/">Vuedle</b-navbar-brand>
+
+    <b-navbar-brand active-class="active" to="/">
+    <img src="@/assets/logo.png">
+    Vuedle
+    </b-navbar-brand>
+
     <b-navbar-toggle target="nav-collapse"/>
     <b-collapse id="nav-collapse" is-nav>
-      <b-nav pills justified>  
+    
+      <b-nav pills justified>  <!-- Left aligned b-nav -->
         <b-nav-item active-class="active" to="/game">Game</b-nav-item>
-        <b-nav-item-dropdown right :text="username"> 
+      </b-nav>
+
+      <b-nav pills justified class="ml-auto"> <!-- Right aligned b-nav -->
+        <b-nav-item-dropdown right toggle-class="text-light ml-auto" :text="username"> 
           <b-dropdown-item v-if="!userid" active-class="active" to="/register">Register</b-dropdown-item>
           <b-dropdown-item v-if="!userid" active-class="active" to="/login">Login</b-dropdown-item>
           <b-dropdown-item v-if="userid" v-on:click="attemptSave()">Save</b-dropdown-item>
           <b-dropdown-item v-if="userid" v-on:click="logOut()">Log Out</b-dropdown-item>
         </b-nav-item-dropdown>
       </b-nav>
+
     </b-collapse>
   </b-navbar>
 </template>
@@ -32,4 +42,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+img {
+  width: 26.5px;
+}
+
+.nav-link{ 
+  color: white;
+}
 </style>
