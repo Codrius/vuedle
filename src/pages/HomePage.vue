5a49c250f6d8146f54cd85854fbfee173015e050
Codrius <magefire342@gmail.com>
2023-09-05 09:42:16 -0400

    Added growth limiter to home jumbotron

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,25 +1,27 @@
 <template>
   <b-container fluid class="home-root m-0">
-    <b-container fluid="sm">
-      <b-jumbotron id="jumbotron1" header="Vuedle" lead="A project by Cody Sewell" text-variant="light" class="pb-3">
-          <hr class="my-4">
-          <b-row class="text-center">
-            <b-col cols="4"><b-button v-b-toggle.accordion-1 variant="dark">Who are you?</b-button></b-col>
-            <b-col cols="4"><b-button v-b-toggle.accordion-2 variant="dark">Who am I?</b-button></b-col>
-            <b-col cols="4"><b-button v-b-toggle.accordion-3 variant="dark">What is Vuedle?</b-button></b-col>
-          </b-row>
-          <b-row class="accordion mt-4" role="tablist">
-            <b-collapse id="accordion-1" accordion="jumbotron-accordion" role="tabpanel">
-              <p class="px-2 mb-0 text-center">My name is Cody Sewell.  I'm a budding web developer who lives in Florida, and I am available to do work for you.  You can contact me at <strong>codysewell1@gmail.com</strong></p>
-            </b-collapse>
-            <b-collapse id="accordion-2" accordion="jumbotron-accordion" role="tabpanel">
-              <p class="px-2 mb-0 text-center">You're the wonderful individual who landed on this website and are about to be impressed by what I was able to learn in only about a month.</p>
-            </b-collapse>
-            <b-collapse id="accordion-3" accordion="jumbotron-accordion" role="tabpanel">
-              <p class="px-2 mb-0 text-center">Vuedle is my portfolio project whose purpose was learning the Redmer Productions development stack.  The result was Vuedle, a Vue based idle game demo, with an Express backend and databased with MongoDB. This website stands as a testament to my initiative and commitment to becoming a valuable member of any development team that would have me.</p>
-            </b-collapse>
-          </b-row>
-      </b-jumbotron>
+    <b-container fluid class="growth-limiter">
+      <b-container fluid="sm">
+        <b-jumbotron id="jumbotron1" header="Vuedle" lead="A project by Cody Sewell" text-variant="light" class="pb-3">
+            <hr class="my-4">
+            <b-row class="text-center">
+              <b-col cols="4"><b-button v-b-toggle.accordion-1 variant="dark">Who are you?</b-button></b-col>
+              <b-col cols="4"><b-button v-b-toggle.accordion-2 variant="dark">Who am I?</b-button></b-col>
+              <b-col cols="4"><b-button v-b-toggle.accordion-3 variant="dark">What is Vuedle?</b-button></b-col>
+            </b-row>
+            <b-row class="accordion mt-4" role="tablist">
+              <b-collapse id="accordion-1" accordion="jumbotron-accordion" role="tabpanel">
+                <p class="px-2 mb-0 text-center">My name is Cody Sewell.  I'm a budding web developer who lives in Florida, and I am available to do work for you.  You can contact me at <strong>codysewell1@gmail.com</strong></p>
+              </b-collapse>
+              <b-collapse id="accordion-2" accordion="jumbotron-accordion" role="tabpanel">
+                <p class="px-2 mb-0 text-center">You're the wonderful individual who landed on this website and are about to be impressed by what I was able to learn in only about a month.</p>
+              </b-collapse>
+              <b-collapse id="accordion-3" accordion="jumbotron-accordion" role="tabpanel">
+                <p class="px-2 mb-0 text-center">Vuedle is my portfolio project whose purpose was learning the Redmer Productions development stack.  The result was Vuedle, a Vue based idle game demo, with an Express backend and databased with MongoDB. This website stands as a testament to my initiative and commitment to becoming a valuable member of any development team that would have me.</p>
+              </b-collapse>
+            </b-row>
+        </b-jumbotron>
+      </b-container>
     </b-container>
   </b-container>
 </template>
@@ -35,7 +37,12 @@
   background-size: cover;
   background-color: rgb(71, 71, 71);
 }
+
 #jumbotron1 {
   background-color: rgba(0,0,0,0.5)
+}
+
+.growth-limiter {
+  max-width: 720px;
 }
 </style>
