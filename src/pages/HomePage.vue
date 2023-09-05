395aee31db89dd557f5103a2944bfe816b8efda8
Codrius <magefire342@gmail.com>
2023-09-05 10:08:38 -0400

    Re-worded home accordions

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,8 +5,8 @@
         <b-jumbotron id="jumbotron1" header="Vuedle" lead="A project by Cody Sewell" text-variant="light" class="pb-3">
             <hr class="my-4">
             <b-row class="text-center">
-              <b-col cols="4"><b-button v-b-toggle.accordion-1 variant="dark">Who are you?</b-button></b-col>
-              <b-col cols="4"><b-button v-b-toggle.accordion-2 variant="dark">Who am I?</b-button></b-col>
+              <b-col cols="4"><b-button v-b-toggle.accordion-1 variant="dark">Who am I?</b-button></b-col>
+              <b-col cols="4"><b-button v-b-toggle.accordion-2 variant="dark">Who are you?</b-button></b-col>
               <b-col cols="4"><b-button v-b-toggle.accordion-3 variant="dark">What is Vuedle?</b-button></b-col>
             </b-row>
             <b-row class="accordion mt-4" role="tablist">
@@ -14,10 +14,10 @@
                 <p class="px-2 mb-0 text-center">My name is Cody Sewell.  I'm a budding web developer who lives in Florida, and I am available to do work for you.  You can contact me at <strong>codysewell1@gmail.com</strong></p>
               </b-collapse>
               <b-collapse id="accordion-2" accordion="jumbotron-accordion" role="tabpanel">
-                <p class="px-2 mb-0 text-center">You're the wonderful individual who landed on this website and are about to be impressed by what I was able to learn in only about a month.</p>
+                <p class="px-2 mb-0 text-center">You're the wonderful individual who landed on this website, and might hopefully give me some feedback on what I can improve around here.  You can find my email address in <strong>"Who am I?"</strong> if you have any suggestions!</p>
               </b-collapse>
               <b-collapse id="accordion-3" accordion="jumbotron-accordion" role="tabpanel">
-                <p class="px-2 mb-0 text-center">Vuedle is my portfolio project whose purpose was learning the Redmer Productions development stack.  The result was Vuedle, a Vue based idle game demo, with an Express backend and databased with MongoDB. This website stands as a testament to my initiative and commitment to becoming a valuable member of any development team that would have me.</p>
+                <p class="px-2 mb-0 text-center">Vuedle is my portfolio project created within the span of about a month whose purpose was learning a web development full stack.  This website was the result; a Vue based frontend with an Express backend, databased using MongoDB. This website stands as a testament to my initiative and ability to become a valuable member of any development team that would have me.</p>
               </b-collapse>
             </b-row>
         </b-jumbotron>
